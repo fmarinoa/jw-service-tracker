f87b179e4fa990c542f747331fbe90574eac09cc
fmarinoa <franco.edson.18014@gmail.com>
2026-06-11 20:42:55 -0500

    fix: remaining type changes

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,5 @@
 export type SessionType = 'house_to_house' | 'revisits' | 'bible_study' | 'other';
 
-export interface User {
-  id: string;
-  name: string;
-  phone: string;
-  preacherType?: string;
-  monthlyGoal?: number;
-}
-
 export interface Entry {
   id: string;
   userId: string;
