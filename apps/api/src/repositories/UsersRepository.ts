f639db9be582ceea238933d8f47cc535013f59e1
Franco Mariño <franco.edson.18014@gmail.com>
2026-07-15 08:52:46 -0500

    feat: add app api

---
+++ apps/api/src/repositories/UsersRepository.ts
@@ -0,0 +1,70 @@
+import bcrypt from "bcrypt";
+import { DateTime } from "luxon";
+
+import { User } from "@/domain/User";
+
+import { BaseRepository, BaseRepositoryProps } from "./BaseRepository";
+
+export class UsersRepository extends BaseRepository {
+  constructor(props: BaseRepositoryProps) {
+    super(props);
+  }
+
+  async findByPhone(phone: string) {
+    return this.handlerCollection(async (collection) => {
+      const result = await collection.findOne({ phone });
+      if (!result) return null;
+      const { _id, ...rest } = result;
+      return new User({ ...rest, id: _id.toString() });
+    });
+  }
+
+  async findById(id: string) {
+    return this.handlerCollection(async (collection) => {
+      const result = await collection.findOne(this.buildIdFilter(id));
+      if (!result) return null;
+      const { _id, ...rest } = result;
+      return new User({ ...rest, id: _id.toString() });
+    });
+  }
+
+  async create(user: User): Promise<User> {
+    return this.handlerCollection(async (collection) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10);
+      const item = {
+        ...this.cleanObject(user),
+        name: user.name.toUpperCase(),
+        password: hashedPassword,
+        createdAt: DateTime.now().toMillis(),
+      };
+      const result = await collection.insertOne(item);
+      return new User({ ...item, id: result.insertedId.toString() });
+    });
+  }
+
+  async update(user: Partial<User>): Promise<User> {
+    return this.handlerCollection(async (collection) => {
+      const id = user.id;
+      if (!id) {
+        throw new Error("User ID is required for update");
+      }
+
+      const updatedAt = DateTime.now().toMillis();
+
+      await collection.updateOne(this.buildIdFilter(id), {
+        $set: {
+          monthlyGoal: user.monthlyGoal,
+          preacherType: user.preacherType,
+          updatedAt,
+        },
+      });
+
+      return new User({
+        id,
+        updatedAt,
+        monthlyGoal: user.monthlyGoal,
+        preacherType: user.preacherType,
+      });
+    });
+  }
+}
---
+++ apps/api/src/repositories/index.ts
@@ -0,0 +1,39 @@
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+
+import { EntriesRepository } from "./EntriesRepository";
+import { UsersRepository } from "./UsersRepository";
+
+const uri = process.env.MONGODB_URI!;
+const options: MongoClientOptions = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+};
+let client: MongoClient;
+
+if (process.env.NODE_ENV === "development") {
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+  if (!globalWithMongo._mongoClient)
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  client = globalWithMongo._mongoClient;
+} else {
+  client = new MongoClient(uri, options);
+}
+
+export const usersRepository = new UsersRepository({
+  client,
+  config: {
+    collectionName: "users",
+  },
+});
+
+export const entriesRepository = new EntriesRepository({
+  client,
+  config: {
+    collectionName: "entries",
+  },
+});
