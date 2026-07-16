1acbfa5963c87dae5c4e32a5046ac712252892e4
Franco Mariño <franco.edson.18014@gmail.com>
2026-07-16 14:52:22 -0500

    ci: add Dockerfiles for API and client

--- apps/client/metro.config.js
+++ apps/client/metro.config.js
@@ -1,6 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Watch the monorepo root (including packages/shared)
+config.watchFolders = [monorepoRoot];
+
+// Resolve dependencies from both the local and monorepo root node_modules
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
 
 module.exports = withNativeWind(config, { input: './src/global.css' });
